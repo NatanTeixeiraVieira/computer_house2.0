68a2314e2a710d36ba50cadb8718a0678e3f497e
CtpaxDS <joaomateus.machado3@gmail.com>
2024-05-07 20:54:30 -0300

    chore:start header

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,12 @@
 import './styles.css';
-
+import { FaSquareInstagram } from "react-icons/fa6";
+import { FaYoutube } from "react-icons/fa";
 export default function Footer() {
-  return <div>Footer João</div>;
+  return <div className="Footer">
+      <FaSquareInstagram>
+        
+      </FaSquareInstagram>
+
+      
+  </div>
 }
