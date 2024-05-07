12170276e4b723c77363302b2585d08a6223eca3
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-05-06 23:45:33 -0300

    feat: Add header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,41 @@
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <nav className="nav-bar">
+        <div className="logo">
+          <h1>
+            Computer <span className="span">House</span>
+          </h1>
+        </div>
+        <nav className="nav-list">
+          <ul>
+            <li>
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="" className="nav-link">
+                Suporte
+              </Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                <ShoppingCartIcon className="shopping-cart" />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="login-button">
+          <button>
+            <a href="#">Entrar</a>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
 }
