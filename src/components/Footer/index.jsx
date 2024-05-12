491881485456cfc1a03f966b5c45aed1a3e9bd63
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-05-11 23:05:41 -0300

    chore: Start footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,10 @@
 import './styles.css';
-
+import { FaSquareInstagram } from 'react-icons/fa6';
+import { FaYoutube } from 'react-icons/fa';
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="Footer">
+      <FaSquareInstagram></FaSquareInstagram>
+    </div>
+  );
 }
